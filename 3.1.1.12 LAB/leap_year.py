a81b376d969075153f7dbb45402e9c450db4ffbf
Sev <90162122+sev2021@users.noreply.github.com>
2022-04-15 01:51:44 +0100

    leap year shorter

--- 3.1.1.12 LAB/leap_year.py
+++ 3.1.1.12 LAB/leap_year.py
@@ -4,11 +4,12 @@ year = int(input("Enter a year: "))
 # Write your code here.
 #	
 
+
 result = "Common year"
 if year < 1582:
     result = "Not within the Gregorian calendar period"
 else:
-    if year % 400 == 0 or year % 4 == 0 and year % 100 != 0:
+    if year % 400 == 0 or year % 4 == 0 and year % 100:
         result = "Leap year"
 
     
