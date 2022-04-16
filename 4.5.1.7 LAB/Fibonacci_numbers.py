3228889aca6320ad7414d81039533807cfe55be5
Sev <90162122+sev2021@users.noreply.github.com>
2022-04-16 16:30:27 +0100

    Fibonacci numbers with memoisation

---
+++ 4.5.1.7 LAB/Fibonacci_numbers.py
@@ -0,0 +1,19 @@
+fib_list = [1,1]  # memoisation
+def fib_fun(n):
+    if n < len(fib_list):
+        return fib_list[n]
+    new_fib = fib_fun(n-1) + fib_fun(n-2)
+    fib_list.append(new_fib)
+    return new_fib
+
+# TESTING
+
+print(fib_fun(14))
+print(fib_fun(15))
+print(fib_fun(16))
+print(fib_fun(17))
+print(fib_fun(18))
+
+# Memoisation output
+
+print(fib_list)
