7a8e4c742cde1fb4666e4faa6c2f158f2512d62b
Sev <90162122+sev2021@users.noreply.github.com>
2022-04-17 01:57:52 +0100

    Factorial recursion and memoisation

--- 4.5.1.8 LAB/factorial_recursiono_memo.py
+++ 4.5.1.8 LAB/factorial_recursiono_memo.py
@@ -1,9 +1,9 @@
 cache = [1,1]  # cache for memoisation
 
 def factorial_function(n):
-    if n < len(cache):
+    if n < len(cache):    # initial cache values cover n < 2 condition
         return cache[n]
-    cache.append(n * factorial_function(n - 1))
+    cache.append(n * factorial_function(n - 1))   # consecutive value
     return cache[n]
     
 print(factorial_function(5))
