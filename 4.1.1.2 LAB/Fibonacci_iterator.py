557cc90cb00f7dc6ee70e763b145ca1aa970986e
Sev <90162122+sev2021@users.noreply.github.com>
2022-04-23 17:28:23 +0100

    Fibonacci numbers within iterable class

--- 4.1.1.2 LAB/Fibonacci_iterator.py
+++ 4.1.1.2 LAB/Fibonacci_iterator.py
@@ -2,19 +2,21 @@
 ## Based on generator/iterator
 
 class Fibo():
+  ## Fibonacci numbers using iterable class
+  
   def __init__(self.n):     ## n-steps of calculation
-    self.n = n
-    self.f1 = self.f2 = 1   ## ancilary variables
+    self.__n = n
+    self.__f1 = self.__f2 = 1   ## ancilary variables
     
   def __iter__(self):       ## __iter__ is requred to initialise iteration
     return self
   
   def __next__(self):       ## __next__ defines way of changing iteration
-    self.f1, self.f2 = self.f2, self.f1 + self.f2
-    self.n -= 1
-    if self.n < 1:
+    self.__f1, self.__f2 = self.__f2, self.__f1 + self.__f2
+    self.__n -= 1
+    if self.__n < 1:
       raise StopIteration   ## to stop iteration MUST USE exception
-    return self.f1        ## return calculated value
+    return self.__f1        ## return calculated value
   
   ## call Fib() in iteration:
   for i in Fibo(12):
